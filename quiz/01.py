885ffbb9cc5ca4dc8b515e3b64c03afd0b723c55
alstn <o162005@gmail.com>
2022-09-17 17:58:08 +0900

    Update 01.py

--- quiz/01.py
+++ quiz/01.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify,request
 app = Flask(__name__)
 
-chain = []
+chain = [
+    {
+        'type':'open',
+        'data':{
+            'id':'0',
+            'question':'Q1'
+            'options':['A1','A2','A3']
+        }
+    }
+]
 cnt = 0
 
 @app.route('/list', methods=['GET'])
