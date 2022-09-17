7f352ac873a38a21148b3dd6853a5796e27ea31b
alstn <o162005@gmail.com>
2022-09-17 17:16:05 +0900

    Update 02. rest_api.py

--- lab_server/02. rest_api.py
+++ lab_server/02. rest_api.py
@@ -1,6 +1,10 @@
 from flask import Flask , jsonify, request
 app = Flask(__name__)
 
+@app.route('/')
+def d():
+    return '시작 페이지'
+
 @app.route('/api1', methods=['GET'])
 def f1():
     return jsonify({'status': 'success'})
